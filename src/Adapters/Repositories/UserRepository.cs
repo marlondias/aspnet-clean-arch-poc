5fa3b52543f34ad3c1d4d74a92d502d01ab4efba
Marlon P. Dias <marlondias@users.noreply.github.com>
2025-06-26 02:36:25 -0300

    feat: Implemented User repository methods

--- src/Adapters/Repositories/UserRepository.cs
+++ src/Adapters/Repositories/UserRepository.cs
@@ -1,45 +1,112 @@
+using CleanArchPOC.Database.Contexts;
 using CleanArchPOC.Domain.Contracts.Repository;
 using CleanArchPOC.Domain.Entities;
 using CleanArchPOC.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using DbUser = CleanArchPOC.Database.Models.User;
 
 namespace CleanArchPOC.Adapters.Services;
 
 public sealed class UserRepository : IUserCommandsRepository, IUserQueriesRepository
 {
+    private readonly MainContext _context;
 
-    public Task Insert(User entity)
+    public UserRepository(MainContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task Update(User entity)
+    public async Task Insert(User entity)
     {
-        throw new NotImplementedException();
+        var dbUser = MapUserToDatabaseModel(entity);
+        _context.Users.Add(dbUser);
+        await _context.SaveChangesAsync();
     }
 
-    public Task DeleteById(int entityId)
+    public async Task Update(User entity)
     {
-        throw new NotImplementedException();
+        var dbUser = await _context.Users.FindAsync(entity.Id);
+
+        if (dbUser is null)
+            throw new InvalidOperationException($"User with ID {entity.Id} not found");
+
+        dbUser.FirstName = entity.Name.FirstName;
+        dbUser.LastName = entity.Name.LastName;
+        dbUser.Email = entity.Email.FullAddress;
+        dbUser.EmailVerifiedAt = entity.EmailVerifiedAt;
+
+        await _context.SaveChangesAsync();
     }
 
-    public Task<User> FindById(int entityId)
+    public async Task DeleteById(int entityId)
     {
-        throw new NotImplementedException();
+        var dbUser = await _context.Users.FindAsync(entityId);
+
+        if (dbUser is null)
+            throw new InvalidOperationException($"User with ID {entityId} not found.");
+
+        _context.Users.Remove(dbUser);
+        await _context.SaveChangesAsync();
     }
 
-    public Task<User> FindByEmail(EmailAddress emailAddress)
+    public async Task<User?> FindById(int entityId)
     {
-        throw new NotImplementedException();
+        var dbUser = await _context.Users.FindAsync(entityId);
+        return dbUser is null ? null : MapUserToDomainEntity(dbUser);
     }
 
-    public Task<User[]> GetAll()
+    public async Task<User?> FindByEmail(EmailAddress emailAddress)
     {
-        throw new NotImplementedException();
+        var dbUser = await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == emailAddress.FullAddress);
+
+        return dbUser is null ? null : MapUserToDomainEntity(dbUser);
     }
 
-    public Task<bool> IsEmailAddressAlreadyInUse(EmailAddress emailAddress)
+    public async Task<User[]> GetAll()
     {
-        throw new NotImplementedException();
+        var dbUsers = await _context.Users.ToArrayAsync();
+        return dbUsers.Select(MapUserToDomainEntity).ToArray();
+    }
+
+    public async Task<bool> IsEmailAddressAlreadyInUse(EmailAddress emailAddress)
+    {
+        return await _context.Users
+            .AnyAsync(u => u.Email == emailAddress.FullAddress);
+    }
+
+    private DbUser MapUserToDatabaseModel(User user)
+    {
+        if (user.HashedPassword is null)
+            throw new InvalidDataException("A User record must have some password.");
+
+        var dbUser = new DbUser()
+        {
+            FirstName = user.Name.FirstName,
+            LastName = user.Name.LastName,
+            Email = user.Email.FullAddress,
+            Password = user.HashedPassword,
+            EmailVerifiedAt = user.EmailVerifiedAt,
+        };
+
+        if (user.Id != null)
+            dbUser.Id = (int)user.Id;
+
+        return dbUser;
+    }
+
+    private User MapUserToDomainEntity(DbUser dbUser)
+    {
+        return new User
+        {
+            Id = dbUser.Id,
+            Name = new PersonName(dbUser.FirstName, dbUser.LastName),
+            Email = new EmailAddress(dbUser.Email),
+            HashedPassword = dbUser.Password,
+            EmailVerifiedAt = dbUser.EmailVerifiedAt,
+            CreatedAt = dbUser.CreatedAt,
+            UpdatedAt = dbUser.UpdatedAt
+        };
     }
 
 }
