49f6ec6e7e5762df41a9e2bc9272dbfd86053cfe
Marlon P. Dias <marlondias@users.noreply.github.com>
2025-06-26 02:42:57 -0300

    feat: Added DI for repo and service

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using CleanArchPOC.Adapters.Services;
+using CleanArchPOC.Database.Contexts;
 using CleanArchPOC.Domain.Contracts.Repository;
 using CleanArchPOC.Domain.Contracts.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IUserCommandsRepository, UserRepository>();
-builder.Services.AddSingleton<IUserQueriesRepository, UserRepository>();
+builder.Services.AddSqlite<MainContext>("Data Source=MainDatabase.db");
+builder.Services.AddScoped<IUserCommandsRepository, UserRepository>();
+builder.Services.AddScoped<IUserQueriesRepository, UserRepository>();
+
 builder.Services.AddSingleton<IStringHashingService, StringHashingService>();
 
 builder.Services.AddControllers();
