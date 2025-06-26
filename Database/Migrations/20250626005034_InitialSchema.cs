97073cf79cb4d964313968fa3b01fda2f26745ce
Marlon P. Dias <marlondias@users.noreply.github.com>
2025-06-25 21:58:45 -0300

    feat: New EF core migration for Users table

---
+++ Database/Migrations/20250626005034_InitialSchema.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchPOC.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    first_name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    last_name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "TEXT", nullable: false),
+                    email_verified_at = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
